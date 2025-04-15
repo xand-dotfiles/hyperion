2d7b6d3dcbf31d8cd14f97a2f3c011b9889b0609
exclusive-and <exclusiveandgate@gmail.com>
2025-04-15 10:26:32 -0400

    Added VR configurations

--- nixos/default.nix
+++ nixos/default.nix
@@ -2,16 +2,31 @@
 
 let
     inherit (inputs.nixpkgs.lib) nixosSystem;
+
+    hyperion = nixosSystem {
+        modules = [
+            ./configuration.nix
+            inputs.gaming.nixosModules.gaming
+            inputs.xand.nixosModules.xand
+            inputs.xmonad.nixosModules.xmonad
+        ];
+    };
+
+    hyperionWithVr = nixosSystem {
+        modules = [
+            ./configuration.nix
+            inputs.gaming.nixosModules.gaming
+            inputs.gaming.nixosModules.vr
+            inputs.xand.nixosModules.xand
+            inputs.xmonad.nixosModules.xmonad
+        ];
+    };
 in
 {
     flake = {
-        nixosConfigurations.hyperion = nixosSystem {
-            modules = [
-                ./configuration.nix
-                inputs.gaming.nixosModules.gaming
-                inputs.xand.nixosModules.xand
-                inputs.xmonad.nixosModules.xmonad
-            ];
+        nixosConfigurations = {
+            inherit hyperion hyperionWithVr;
+            default = hyperion;
         };
     };
 }
